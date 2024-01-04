473f3f4a7b1d19414a243ab3e7a73ab696080ee8
HyungJu19 <HyungJu911@gmail.com>
2024-01-04 22:07:00 +0900

    #민호

---
+++ twoTour/src/main/java/com/lec/twotour/TwoTourApplication.java
@@ -0,0 +1,13 @@
+package com.lec.twotour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoTourApplication.class, args);
+	}
+
+}
